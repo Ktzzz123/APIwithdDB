6c5f24a4ad1c66071641fb35337acc67c359dc98
TieuThiKieuTien/18521491 <18521491@gm.uit.edu.vn>
2021-12-06 07:17:14 +0700

    add product's router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app =express();
 const dotenv=require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
-
+app.use("/api/product", productRoute)
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("server running!!")
 }) 
--- routes/user.js
+++ routes/user.js
@@ -49,11 +49,39 @@ router.get("/find/:id",verifyTokenAndAdmin, async (req, res)=>{
 
 // get all user
 router.get("/",verifyTokenAndAdmin, async (req, res)=>{
+  const query = req.query.new
   try{
-    const user = await User.find();
-  
+    const users = query 
+    ?  await User.find().limit(3).sort({_id:-1})
+    : await User.find();
 
-    res.status(200).json(user);
+    res.status(200).json(users);
+  }catch(err){
+    res.status(500).json(err)
+  }
+})
+
+//get user stats
+router.get("/stats", verifyTokenAndAdmin, async (req, res)=>{
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear()-1));
+  try{
+    const data = await User.aggregate([
+      {$match: {createdAt:{$gte:lastYear }}},
+      {
+        $project:{
+          month:{$month: "$createdAt"},
+        }
+      },
+      {
+        $group:{
+          _id: "$month",
+          total: { $sum:1}
+        }
+      }
+    ])
+
+    res.status(200).json(data)
   }catch(err){
     res.status(500).json(err)
   }
